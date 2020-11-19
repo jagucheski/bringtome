acb9083a9f06d9e5e025ab084c9e64ac304d8997
maicon.jagucheski <maicon.jagucheski@PMP012003.pmparobe.local>
2020-11-19 10:56:01 -0300

    Initial commit.

---
+++ src/main/java/br/com/jagucheski/bringtome/BringtomeApplication.java
@@ -0,0 +1,13 @@
+package br.com.jagucheski.bringtome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BringtomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BringtomeApplication.class, args);
+	}
+
+}
